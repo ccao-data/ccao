4b971682f62eea96db6d67eb987eee6b32935c16
Dan Snow <DSnow@cookcountyassessor.com>
2020-06-18 11:42:51 -0500

    Update spatial datasets with leaflet compatible datum

--- data-raw/nbhd_shp.R
+++ data-raw/nbhd_shp.R
@@ -1,4 +1,6 @@
 # Load shapes and save them as .rda
-nbhd_shp <- sf::st_read("data-raw/nbhd_shp.geojson")
+nbhd_shp <- sf::st_read("data-raw/nbhd_shp.geojson") %>%
+  sf::st_transform(4326) %>%
+  mutate_if(is.factor, as.character)
 
 usethis::use_data(nbhd_shp, overwrite = TRUE, compress = "xz")
--- data-raw/town_shp.R
+++ data-raw/town_shp.R
@@ -1,4 +1,6 @@
 # Load shapes and save them as .rda
-town_shp <- sf::st_read("data-raw/town_shp.geojson")
+town_shp <- sf::st_read("data-raw/town_shp.geojson") %>%
+  sf::st_transform(4326) %>%
+  mutate_if(is.factor, as.character)
 
 usethis::use_data(town_shp, overwrite = TRUE, compress = "xz")
