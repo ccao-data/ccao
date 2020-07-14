deafe9b782608a452e4358fd1f8db7b3aa1506d8
Dan Snow <DSnow@cookcountyassessor.com>
2020-07-13 23:40:04 -0500

    Fix GAR1_SIZE being additive rather than replacement

--- data-raw/chars_cols.R
+++ data-raw/chars_cols.R
@@ -2,11 +2,11 @@ chars_cols <- dplyr::lst(
 
   # List of columns in dataset that will need to have ADDCHARS values added
   add_target = c(
-    "APTS", "ROOMS", "BEDS", "FBATH", "HBATH", "GAR1_SIZE", "BLDG_SF"
+    "APTS", "ROOMS", "BEDS", "FBATH", "HBATH", "BLDG_SF"
   ),
   add_source = c(
-    "QU_NUM_APTS", "QU_ROOMS", "QU_BEDS", "QU_FULL_BATH", "QU_HALF_BATH",
-    "QU_GARAGE_SIZE", "QU_SQFT_BLD"
+    "QU_NUM_APTS", "QU_ROOMS", "QU_BEDS",
+    "QU_FULL_BATH", "QU_HALF_BATH", "QU_SQFT_BLD"
   ),
 
   # List of columns in dataset that will need to have ADDCHARS values replaced
@@ -14,7 +14,7 @@ chars_cols <- dplyr::lst(
     "TYPE_RESD", "USE", "EXT_WALL", "ROOF_CNST", "BSMT", "BSMT_FIN", "HEAT",
     "AIR", "FRPL", "ATTIC_TYPE", "ATTIC_FNSH", "TP_PLAN", "TP_DSGN",
     "CNST_QLTY", "RENOVATION", "SITE", "GAR1_CNST", "GAR1_ATT", "GAR1_AREA",
-    "PORCH", "REPAIR_CND", "CLASS"
+    "PORCH", "REPAIR_CND", "GAR1_SIZE"
   ),
 
   rep_source = c(
@@ -23,7 +23,7 @@ chars_cols <- dplyr::lst(
     "QU_FIRE_PLACE", "QU_ATTIC_TYPE", "QU_ATTIC_FINISH", "QU_TYPE_PLAN",
     "QU_TYPE_DESIGN", "QU_CONSTRUCT_QUALITY", "QU_RENOVATION", "QU_SITE_DESIRE",
     "QU_GARAGE_CONST", "QU_GARAGE_ATTACHED", "QU_GARAGE_AREA", "QU_PORCH",
-    "QU_STATE_OF_REPAIR", "QU_CLASS"
+    "QU_STATE_OF_REPAIR", "QU_GARAGE_SIZE"
   )
 )
 
