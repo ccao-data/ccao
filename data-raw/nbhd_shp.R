ba928d3eb9d0efc907cbd15bc2812f88c22d156b
Dan Snow <DSnow@cookcountyassessor.com>
2020-08-09 01:07:49 -0500

    Fix errors with st_transform()
    See https://github.com/r-spatial/sf/issues/1419 for details

--- data-raw/nbhd_shp.R
+++ data-raw/nbhd_shp.R
@@ -3,6 +3,7 @@ nbhd_shp <- sf::st_read("data-raw/nbhd_shp.geojson") %>%
   sf::st_transform(4326) %>%
   mutate_if(is.factor, as.character) %>%
   mutate_if(is.numeric, as.character) %>%
-  arrange(township_code, nbhd)
+  arrange(township_code, nbhd) %>%
+  sf::st_set_crs(4326)
 
 usethis::use_data(nbhd_shp, overwrite = TRUE, compress = "xz")
--- data-raw/town_shp.R
+++ data-raw/town_shp.R
@@ -3,6 +3,7 @@ town_shp <- sf::st_read("data-raw/town_shp.geojson") %>%
   sf::st_transform(4326) %>%
   mutate_if(is.factor, as.character) %>%
   mutate_if(is.numeric, as.character) %>%
-  arrange(township_code)
+  arrange(township_code) %>%
+  sf::st_set_crs(4326)
 
 usethis::use_data(town_shp, overwrite = TRUE, compress = "xz")
