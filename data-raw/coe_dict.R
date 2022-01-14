f66a0fd7fd3fa5dbb8c8047dbd6fb72b396f5406
Dan Snow <daniel.snow@cookcountyil.gov>
2022-01-13 20:46:55 -0600

    Specify readr:: dependency explicity

--- data-raw/coe_dict.R
+++ data-raw/coe_dict.R
@@ -1,6 +1,9 @@
 coe_dict <- readr::read_csv(
   file = "data-raw/coe_dict.csv",
-  col_types = cols(coe_code = col_integer(), coe_reason = col_character())
+  col_types = readr::cols(
+    coe_code = readr::col_integer(),
+    coe_reason = readr::col_character()
+  )
 )
 
 usethis::use_data(coe_dict, overwrite = TRUE)
