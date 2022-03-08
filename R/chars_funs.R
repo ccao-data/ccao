d4442fd47bcb9d38503260a838ce4111341f6fc9
Dan Snow <daniel.snow@cookcountyil.gov>
2022-03-08 15:08:12 -0600

    Re-add updated chars_ 288 functions

--- R/chars_funs.R
+++ R/chars_funs.R
@@ -148,3 +148,206 @@ chars_288_active <- function(start_year, town) {
 
   return(out)
 }
+
+
+#' Convert ADDCHARS SQL data to a sparse format that can be joined to other data
+#'
+#' @description The ADDCHARS SQL table includes individual rows listing the PIN,
+#' start date, and characteristic updates associated with a 288 Home Improvement
+#' Exemption. This data format is difficult to work with and complicated by the
+#' fact that multiple 288s can be active at the same time for different periods,
+#' and some columns from ADDCHARS add to existing characteristics, while some
+#' overwrite existing characteristics. Additionally, 288s can be active for
+#' multi-code properties, meaning that one building on a PIN could have an
+#' improvement while the others remain untouched.
+#'
+#' The goal of this function is to transform these single row records into a
+#' sparse data frame which lists a row and characteristic update per PIN
+#' per YEAR.
+#'
+#' The transformation caused by this function is most easily visualized
+#' with a mock dataset.
+#'
+#' The base ADDCHARS data looks like this:
+#'
+#' | qu_pin     | year | qu_town | qu_upload_date | qu_sqft_bld | qu_rooms |
+#' |------------|------|---------|----------------|-------------|----------|
+#' | 12345      | 2013 | 77      | 130702         | 200         | 0        |
+#' | 12345      | 2015 | 77      | 150703         | 300         | 1        |
+#'
+#' This function will transform it into this:
+#'
+#' | qu_pin | year | qu_sqft_bld | qu_rooms |
+#' |--------|------|-------------|----------|
+#' | 12345  | 2013 | 200         | 0        |
+#' | 12345  | 2014 | 200         | 0        |
+#' | 12345  | 2015 | 500         | 1        |
+#' | 12345  | 2016 | 500         | 1        |
+#' | 12345  | 2017 | 500         | 1        |
+#' | 12345  | 2018 | 300         | 1        |
+#' | 12345  | 2019 | 300         | 1        |
+#' | 12345  | 2020 | 300         | 1        |
+#'
+#' Each PIN will have a row for each year that a 288 is active and a column
+#' for each characteristic specified by \code{additive_source} and
+#' \code{replacement_source}. This table can be easily left joined to modeling
+#' data to update characteristic values.
+#'
+#' @param data A data frame containing ADDCHARS columns.
+#' @param pin_col A column name specifying the PIN column.
+#' @param year_col A column name specifying the year column.
+#' @param town_col A column name specifying the town column. This is used to
+#'   determine the length of the 288, given the starting date specified
+#'   by \code{year_col}.
+#' @param upload_date_col A column name specifying the upload date of the 288,
+#'   this is use to determine which replacement characteristics take precedence
+#'   if multiple 288s were filed in the same year (the latest one is chosen).
+#' @param additive_source A tidyselect selection of columns which contains
+#'   additive characteristic values. These are values such as square feet which
+#'   get ADDED to existing characteristics. For example, if qu_sqft_bld is equal
+#'   to 100, then 100 sqft get added to the existing square footage of the
+#'   property. The easiest way to specify all additive columns is to use the
+#'   built-in crosswalk, \code{any_of(ccao::chars_cols$add$source)}.
+#' @param replacement_source A tidyselect selection of columns which contains
+#'   replacement characteristic values. These are values such as number of rooms
+#'   which get OVERWRITE existing characteristics. For example, if QU_ROOMS is
+#'   equal to 3, then the property will be update to have 3 total rooms.
+#'   The easiest way to specify all replacement columns is to use the
+#'   built-in crosswalk, \code{any_of(ccao::chars_cols$replace$source)}.
+#'
+#' @note Use dplyr/tidyselect syntax for specifying column names. For example,
+#'   use \code{qu_sqft_bld} instead of \code{"qu_sqft_bld"}, or use a tidyselect
+#'   function such as \code{starts_with("qu_")}.
+#'
+#' @return A sparsified data frame of characteristic updates per PIN per year.
+#' See example.
+#'
+#' @examples
+#'
+#' # Load the included example dataset
+#' data("chars_sample_hie")
+#'
+#' chars_sparsify(
+#'   chars_sample_hie[1:3, ],
+#'   pin_col = pin,
+#'   year_col = year,
+#'   town_col = as.character(qu_town),
+#'   upload_date_col = qu_upload_date,
+#'   additive_source = any_of(chars_cols$add$source),
+#'   replacement_source = any_of(chars_cols$replace$source)
+#' )
+#' @md
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data :=
+#' @family chars_funs
+#' @export
+chars_sparsify <- function(data, pin_col, year_col, town_col, upload_date_col,
+                           additive_source, replacement_source) {
+
+  # Goal here is to transform our data from single rows specifying 288s that
+  # are active for X years to a dataset of X rows with the characteristic
+  # updates for each year. To do this, we first eliminate multiple 288s active
+  # on the same pin, year, and class (say someone made 2 improvements in 1 year)
+  # by summing the additive characteristics and taking the latest replacement
+  # ones
+  data %>%
+    dplyr::group_by({{ pin_col }}, {{ year_col }}) %>%
+    dplyr::arrange({{ upload_date_col }}, .by_group = TRUE) %>%
+    dplyr::summarize(
+      dplyr::across({{ additive_source }}, sum),
+      dplyr::across({{ replacement_source }}, last_nonzero_element),
+      town = dplyr::first({{ town_col }})
+    ) %>%
+    # Next, for each single 288 row, we determine the years it will be active
+    # by taking the start year and town. This list gets put into a new variable
+    # and then expanded using unnest(), which creates a duplicate row for each
+    # value of the list
+    dplyr::mutate({{ year_col }} := ccao::chars_288_active(
+      as.numeric({{ year_col }}),
+      as.character(.data$town)
+    )) %>%
+    tidyr::unnest({{ year_col }}) %>%
+    # The number of rows in our dataset is now equal to # of unique 288s * # of
+    # years active for each PIN. Our final step is to merge the 288
+    # characteristic updates by PIN for years with overlapping 288s.
+    # For each year a PIN has active 288s, we sum the additive chars
+    # and take the last nonzero value for replacement
+    dplyr::group_by({{ pin_col }}, {{ year_col }}) %>%
+    dplyr::arrange({{ year_col }}, .by_group = TRUE) %>%
+    dplyr::summarize(
+      dplyr::across({{ additive_source }}, sum),
+      dplyr::across({{ replacement_source }}, last_nonzero_element),
+      hie_num_active = dplyr::n()
+    ) %>%
+    dplyr::ungroup()
+}
+
+
+#' Update characteristic values using joined values from ADDCHARS
+#'
+#' @description Function used to update the characteristic values of a
+#' data frame containing both the original characteristic value (from Athena)
+#' and the value to add or replace it with (ADDCHARS). This function expects a
+#' data frame formatted using \code{\link{chars_sparsify}}.
+#'
+#' @param data A data frame containing  columns AND their equivalent
+#'   ADDCHARS columns.
+#' @param additive_target A tidyselect selection of Athena column names
+#'   that should have characteristics added to them from their equivalent
+#'   ADDCHARS column, which must also be present.
+#' @param replacement_target A tidyselect selection of Athena column names
+#'   that should have characteristics replaced using their equivalent ADDCHARS
+#'   column, which must also be present.
+#'
+#' @return A data frame with updated additive and replacement target columns.
+#'
+#' @importFrom magrittr %>%
+#' @family chars_funs
+#' @export
+chars_update <- function(data, additive_target, replacement_target) {
+
+  # Given an input dataset, this code will lookup each of the Athena
+  # columns specified and ADD or REPLACE their values using the equivalent
+  # ADDCHARS column
+  data %>%
+    dplyr::mutate(
+      dplyr::across(
+        {{ additive_target }},
+        function(x) {
+          source_col <- dplyr::cur_data()[[chars_get_col(dplyr::cur_column())]]
+          rowSums(cbind(x, source_col), na.rm = TRUE)
+        }
+      ),
+      dplyr::across(
+        {{ replacement_target }},
+        function(x) {
+          source_col <- dplyr::cur_data()[[chars_get_col(dplyr::cur_column())]]
+          idx <- !is.na(source_col) & source_col != 0
+          replace(x, which(idx), source_col[idx])
+        }
+      )
+    )
+}
+
+
+# Unexported utility function to lookup equivalent ADDCHARS column given
+# an Athena column
+chars_get_col <- function(col) {
+  source_cols <- c(ccao::chars_cols$add$source, ccao::chars_cols$replace$source)
+  target_cols <- c(ccao::chars_cols$add$target, ccao::chars_cols$replace$target)
+  source_cols[which(target_cols %in% col)]
+}
+
+
+# Get the last nonzero element of a vector. Used to get the latest
+# "replacement" characteristic for each 288
+last_nonzero_element <- function(x) {
+  x_class <- class(x)
+  if (x_class == "character") {
+    idx <- x != "0"
+    ifelse(any(idx), x[max(which((idx)))], "0")
+  } else {
+    idx <- x != 0
+    ifelse(any(idx), x[max(which((idx)))], 0)
+  }
+}
--- tests/testthat/test-chars_funs.R
+++ tests/testthat/test-chars_funs.R
@@ -45,21 +45,100 @@ test_that("bad input data stops execution", {
 })
 
 
-##### TEST chars_fix_age() #####
+##### TEST chars_sparsify() #####
 
-context("test chars_fix_age()")
+context("test chars_sparsify()")
+
+# Create simple test case using fake data
+chars_sparsify_simple_data <- dplyr::tibble(
+  pin = c("123456", "123456", "123456"),
+  year = c(2013, 2015, 2020),
+  qu_class = c("206", "206", "206"),
+  qu_town = c("25", "25", "25"),
+  qu_upload_date = c(3, 4, 3),
+  qu_beds = c(1, 0, 2),
+  qu_sqft_bld = c(300, 100, 200),
+  qu_garage_size = c(0, 3, 0)
+)
+
+# Create the correct, expected output
+chars_sparsify_simple_correct <- dplyr::tibble(
+  pin = rep("123456", 11),
+  year = c(2013:2018, 2020:2024),
+  qu_beds = c(rep(1, 6), rep(2, 5)),
+  qu_sqft_bld = c(rep(300, 2), rep(400, 4), rep(200, 5)),
+  qu_garage_size = c(0, 0, rep(3, 4), rep(0, 5)),
+  qu_class = rep("206", 11),
+  hie_num_active = c(1, 1, rep(2, 4), rep(1, 5))
+)
+
+# Create results for simple case
+chars_sparsify_simple_test <- chars_sparsify_simple_data %>%
+  chars_sparsify(
+    pin_col = pin,
+    year_col = year,
+    town_col = as.character(qu_town),
+    upload_date_col = qu_upload_date,
+    additive_source = any_of(chars_cols$add$source),
+    replacement_source = any_of(chars_cols$replace$source)
+  )
+
+# Create complex case using sample dataset
+chars_sample_hie_sparse <- chars_sample_hie %>%
+  chars_sparsify(
+    pin_col = pin,
+    year_col = year,
+    town_col = qu_town,
+    upload_date_col = qu_upload_date,
+    additive_source = any_of(chars_cols$add$source),
+    replacement_source = any_of(chars_cols$replace$source)
+  )
+
+# Test dataset equivalence
+test_that("data is identical to known good output", {
+  expect_equivalent(
+    chars_sparsify_simple_test,
+    chars_sparsify_simple_correct
+  )
+  expect_known_hash(chars_sample_hie_sparse, hash = "0276a27ad5")
+})
+
+
+##### TEST chars_update() #####
+
+context("test chars_update()")
 
 # Test mock dataset with only addchars columns
 chars_fake_universe <- dplyr::tibble(
-  PIN = rep("123456", 11),
-  TAX_YEAR = c(2013:2018, 2020:2024),
-  TOWN = rep("25", 11),
-  AGE = c(rep(80, 3), rep(83, 3), rep(86, 2), rep(89, 3)),
-  BEDS = c(rep(2, 6), rep(3, 5)),
-  BLDG_SF = c(600, 600, rep(700, 4), rep(1100, 5)),
-  GAR1_SIZE = c(rep(0, 6), rep(3, 5)),
-  CLASS = rep("206", 11)
+  pin = rep("123456", 11),
+  year = c(2013:2018, 2020:2024),
+  town = rep("25", 11),
+  char_age = c(rep(80, 3), rep(83, 3), rep(86, 2), rep(89, 3)),
+  char_beds = c(rep(2, 6), rep(3, 5)),
+  char_bldg_sf = c(600, 600, rep(700, 4), rep(1100, 5)),
+  char_gar1_size = c(rep(0, 6), rep(3, 5)),
+  meta_class = rep("206", 11)
 )
+
+chars_fake_updated <- chars_fake_universe %>%
+  dplyr::left_join(
+    chars_sparsify_simple_test,
+    by = c("pin", "year")
+  ) %>%
+  chars_update(
+    additive_target = any_of(ccao::chars_cols$add$target),
+    replacement_target = any_of(ccao::chars_cols$replace$target)
+  )
+
+# Test that output is identical to previous output
+test_that("data is identical to known good output", {
+  expect_known_hash(chars_fake_updated, hash = "a4b0543b65")
+})
+
+
+##### TEST chars_fix_age() #####
+
+context("test chars_fix_age()")
 
 age_test <- dplyr::tibble(
   age = c(120, 120, 123),
@@ -74,9 +153,9 @@ test_that("output is as expected", {
   )
   expect_equal(
     chars_fix_age(
-      chars_fake_universe$AGE,
-      chars_fake_universe$TAX_YEAR,
-      chars_fake_universe$TOWN
+      chars_fake_universe$char_age,
+      chars_fake_universe$year,
+      chars_fake_universe$town
     ),
     c(80:85, 87:91)
   )
