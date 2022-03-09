3bff8f2a657ef895a1acf5d31d8d650b75b34b02
Dan Snow <daniel.snow@cookcountyil.gov>
2022-03-09 11:50:04 -0600

    Update vars_dict

--- R/data.R
+++ R/data.R
@@ -181,7 +181,7 @@
 #' to their human-readable value (ROOF_CNST = 1
 #' becomes ROOF_CNST = Shingle/Asphalt).
 #'
-#' @format A data frame with 447 rows and 17 variables:
+#' @format A data frame with 467 rows and 19 variables:
 #' \describe{
 #'   \item{var_from_source}{Name of the raw data source that the variable
 #'   was extracted from}
