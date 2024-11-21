2549daf84b256252fca490287d9bc26dec7ea34e
Jean Cochrane <jean@jeancochrane.com>
2024-11-21 16:47:08 -0600

    Better path management in docs conf.py

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -1,13 +1,14 @@
-import os
+import pathlib
 import sys
 
 from sphinx_pyproject import SphinxConfig
 
 # Add source path to sys path so that autodoc can load functions
-sys.path.append(os.path.abspath("../.."))
+rootdir = pathlib.Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(rootdir.resolve()))
 
 # Loads config from pyproject.toml
-config = SphinxConfig("../../pyproject.toml", globalns=globals())
+config = SphinxConfig(rootdir / "pyproject.toml", globalns=globals())
 
 # Options that can't be parsed by sphinx-pyproject
 html_sidebars = {"**": []}
