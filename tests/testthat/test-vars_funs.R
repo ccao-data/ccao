e75a336e0248298958c76a4faff618b0c46ae75a
Jean Cochrane <jean@jeancochrane.com>
2024-12-03 11:26:59 -0600

    Remove deprecated `vars_check_class` function from R package

--- tests/testthat/test-vars_funs.R
+++ tests/testthat/test-vars_funs.R
@@ -1,38 +1,3 @@
-context("test vars_check_class()")
-
-##### TEST vars_check_class() #####
-
-# Test for expected outputs
-test_that("output is as expected", {
-  expect_equal(vars_check_class(50, 800, "202"), TRUE)
-  expect_equal(vars_check_class(50, 800, c("202", "299")), c(TRUE, TRUE))
-  expect_equal(vars_check_class(50, 800, c("202", "203")), c(TRUE, FALSE))
-  expect_equal(vars_check_class(62, 3000, c("206", "210")), c(FALSE, FALSE))
-  expect_equal(vars_check_class(c(62, 63), 800, "210"), c(FALSE, TRUE))
-  expect_equal(vars_check_class(-10, 200, "206"), NA)
-})
-
-test_that("invalid matches return false", {
-  expect_false(vars_check_class(50, 800, "190"), FALSE)
-  expect_false(vars_check_class(60, 1000, "202"))
-  expect_false(vars_check_class(62, 1500, "205"))
-})
-
-
-# Test that invalid inputs throw errors
-test_that("invalid data types stop process", {
-  expect_condition(vars_check_class("50", 800, 202))
-  expect_condition(vars_check_class(50, "800", "202"))
-  expect_condition(vars_check_class(50, 800, 202))
-})
-
-test_that("invalid vector lengths stop process", {
-  expect_condition(vars_check_class(50, c(800, 1000, 2000), c("203", "204")))
-  expect_condition(vars_check_class(c(50, 80), c(800, 1000, 2000), "204"))
-  expect_condition(vars_check_class(5, c(800, 1000), c("203", "204", "297")))
-})
-
-
 context("test vars_rename()")
 
 ##### TEST vars_rename() #####
