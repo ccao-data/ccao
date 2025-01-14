41eac0e04b8c7512edef33f83079c83c362b3b3d
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2025-01-14 19:59:14 +0000

    Rename validate-vars-dict -> check-vars-dict

--- scripts/check-vars-dict.R
+++ scripts/check-vars-dict.R
@@ -1,16 +1,16 @@
+# Script to check that the `vars_dict` data object is well-formed
 load("data/vars_dict.rda")
 
+# Check for duplicate model parameters
 non_na_model_vars <- subset(
   vars_dict,
   !is.na(var_name_model)
 )[c("var_name_model", "var_code", "var_value")]
-
 dupes <- non_na_model_vars[which(duplicated(non_na_model_vars)), ]
 
 if (nrow(dupes) > 0) {
-  dupe_var_names <- dupes$var_name_model
   stop(
     "Duplicate var_name_model entries in vars_dict: ",
-    paste(dupe_var_names, collapse = ", ")
+    paste(dupes$var_name_model, collapse = ", ")
   )
 }
